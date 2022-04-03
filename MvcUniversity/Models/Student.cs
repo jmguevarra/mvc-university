97f9cc5f1cafdac0897067c65244f906405e9fa8
jmguevarra <jaimeguevarra22@gmail.com>
2022-04-04 09:32:52 +1000

    Add project files.

---
+++ MvcUniversity/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcUniversity.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+
+        [DisplayName("Student LRN")]
+        public string StudLRN { get; set; }
+        
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+
+        [DisplayName("Course")]
+        public string StudCourse { get; set; }
+
+        [DisplayName("Year")]
+        public int StudYear { get; set; }
+
+        [DisplayName("Section")]
+        public string StudSection { get; set; }
+
+    }
+}
