db6326670f11e2eadd5f9f8e75beca3363fa9fe1
jmguevarra <jaimeguevarra22@gmail.com>
2022-04-07 18:31:34 +1000

    Json Options

--- MvcUniversity/Controllers/TeachersController.cs
+++ MvcUniversity/Controllers/TeachersController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using MvcUniversity.Data;
 using MvcUniversity.Models;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace MvcUniversity.Controllers
 {
@@ -12,15 +14,22 @@ namespace MvcUniversity.Controllers
     {
         private readonly MvcUniversityDbContext _db;
         private IEnumerable<Teacher> teachers;
+        private JsonOptions jsonOptions;
 
         public TeachersController(MvcUniversityDbContext db)
         {
             this._db = db;
             this.teachers = this._db.Teacher;
+            this.jsonOptions = JsonConvert.DeserializeObject<JsonOptions>("");
         }
         public IActionResult Index()
         {
             return View(this.teachers);
+        }
+
+        public IActionResult Edit() {
+
+            return View();
         }
     }
 }
---
+++ MvcUniversity/Models/JsonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcUniversity.Models
+{
+    public class JsonModel
+    {
+
+        public class Options { 
+            public string[] gender { get; set; }
+            
+            public string[] yearLevel { get; set; }
+        }
+    }
+}
